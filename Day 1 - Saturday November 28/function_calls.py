80a553f165b31fd68b46bbdacf92799d1fe0872a
yorkuscix <explore@yorku.ca>
2020-11-27 11:34:17 -0500

    Updated Day 1 example code

--- Day 1 - Saturday November 28/function_calls.py
+++ Day 1 - Saturday November 28/function_calls.py
@@ -1,17 +1,43 @@
 import math
 
-def a(n):
+def l(word: str):
+    total = 0
+    for letter in word:
+        total = total + 1
+    return total
+
+def a(word: str):
+    total = 0
+    for letter in word:
+        if letter == 'a' or letter == 'A':
+            total = total + 1
+    return total
+
+def v(word: str):
+    total = 0
+    for letter in word:
+        if letter in 'aeiouAEIOU':
+            total = total - 1
+        if letter not in 'aeiouAEIOU':
+            total = total + 1
+    return total
+
+def f(n: int):
     lst = []
     for i in range(n):
         lst.append(i)
-    t = f(lst)
-    return t
+    result = s(lst)
+    return math.sqrt(result)
 
-def f(new_lst):
-    t = 0
+def s(new_lst: list):
+    total = 0
     for i in new_lst:
-        t = t + g(i)
-    return math.sqrt(t)
+        total = total + i
+    return total
 
-def g(item):
-    return item * item
+def d(num: float):
+    count = 0
+    while num > 1:
+        num = num/2
+        count = count + 1
+    return count
