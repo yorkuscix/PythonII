e009cd9c9836b08914a19c1e772c8a2df9f15e8f
yorkuscix <explore@yorku.ca>
2020-12-01 17:37:35 -0500

    updated day 2 code

--- Day 2 - Tuesday December 1/cards.py
+++ Day 2 - Tuesday December 1/cards.py
@@ -1,2 +1,9 @@
 suits = ['Spade', 'Heart', 'Club', 'Diamond']
 values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+def generate_deck():
+    deck = []
+    for suit in suits:
+        for value in values:
+            deck.append(value + suit)
+    return deck
