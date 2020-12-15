20c851b693f637e08a1696210208ddca248574e0
yorkuscix <explore@yorku.ca>
2020-12-15 15:51:10 -0500

    Added day 6 starter code

---
+++ Day 6 - Tuesday December 15/isometry_starter.py
@@ -0,0 +1,34 @@
+import turtle
+
+def draw_box():
+    turtle.penup()
+    turtle.setpos(0, 0)
+    turtle.pendown()
+    
+    turtle.color("green")
+    turtle.begin_fill()
+    turtle.setpos(87, 50)
+    turtle.setpos(87, 150)
+    turtle.setpos(0, 100)
+    turtle.setpos(0, 0)
+    turtle.end_fill()
+    
+    turtle.color("grey")
+    turtle.begin_fill()
+    turtle.setpos(-87, 50)
+    turtle.setpos(-87, 150)
+    turtle.setpos(0, 100)
+    turtle.setpos(0, 0)
+    turtle.end_fill()
+    
+    turtle.penup()
+    turtle.setpos(0, 100)
+    turtle.pendown()
+    
+    turtle.color("orange")
+    turtle.begin_fill()
+    turtle.setpos(87, 150)
+    turtle.setpos(0, 200)
+    turtle.setpos(-87, 150)
+    turtle.setpos(0, 100)
+    turtle.end_fill()
